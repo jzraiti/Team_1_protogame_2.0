7fc002fe8fc0258df10e8b6ee1b7bd4a53fb47fb
Justin Williams <jw1116@live.com>
2019-11-11 21:59:43 -0500

    added a scene that test a fire particle effect and a new project file that test shooting particles

---
+++ New Unity Project/Assets/laserbeam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserbeam : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+    ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = particleSystem;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            print("Fire1");
+            ps.Emit(1);
+        }
+    }
+
+}
