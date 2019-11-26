d6e09726f1e98dab0aa0b04078ec383648cb5557
Jason Raiti <jzraiti@gmail.com>
2019-11-26 11:49:51 -0500

    Health System?

---
+++ Team_01_protogame_2.0/Assets/Jasons Stuff/Scripts/HealthScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+
+    public float health;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        print("Collision!");
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            health -= 10;
+            print(health);
+        }
+
+
+    }
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
