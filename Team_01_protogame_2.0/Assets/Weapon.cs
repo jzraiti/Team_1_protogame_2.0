57018b0bb1c8b80242e42f10f31347c868596aa6
Justin Williams <jw1116@live.com>
2019-12-01 18:17:57 -0500

    Added camera movement and shooting mechanic

---
+++ Team_01_protogame_2.0/Assets/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject pingPong;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+            
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            animator.SetBool("IsThrowing", false);
+        }
+    }
+
+       void Shoot()
+        {
+        animator.SetBool("IsThrowing", true);
+        Instantiate(pingPong, firePoint.position, firePoint.rotation);
+        //animator.SetBool("IsThrowing", false);
+     
+        }
+}
---
+++ Team_01_protogame_2.0/Assets/enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject deathEffect;
+    public int damage = 10;
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        //Instantiate(deathEffect, transform.position, Quaternion.identity);
+        //Destroy(gameObject);
+        gameObject.SetActive(false);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
