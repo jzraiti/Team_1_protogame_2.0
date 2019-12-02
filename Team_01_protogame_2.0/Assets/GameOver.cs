ca0274f2958afc08a2ea33a92d88942678509476
Justin Williams <jw1116@live.com>
2019-12-01 20:47:43 -0500

    created scenes for level 1 and level 2

--- Team_01_protogame_2.0/Assets/GameOver.cs
+++ Team_01_protogame_2.0/Assets/GameOver.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 
 }
